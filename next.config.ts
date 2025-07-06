e68ed763d539c5115d068b3a659dc5b50b72f600
Ahmed <m.ahmed.arif>
2025-07-06 19:04:34 +0200

    Fix ESLint for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 // next.config.ts
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
